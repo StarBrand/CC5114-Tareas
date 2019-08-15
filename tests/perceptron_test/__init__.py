84ac91e97c47e2fbb2a64c2f673a3909fe10a914
StarBrand <juan.saez.hidalgo@gmail.com>
2019-08-14 23:15:15 -0400

    Change test name to make visible for unnitest

--- tests/perceptron_test/__init__.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from tests.perceptron_test.p_and import PerceptronAnd
-from tests.perceptron_test.p_or import PerceptronOr
-from tests.perceptron_test.p_nand import PerceptronNand
-from tests.perceptron_test.p_sum import PerceptronSum
-
-
-if __name__ == "__main__":
-    unittest.main()
