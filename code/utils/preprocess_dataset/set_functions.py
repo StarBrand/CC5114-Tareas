4acc0038b2ea0f627ea518e999daa23aaaefcb8f
StarBrand <juan.saez.hidalgo@gmail.com>
2019-08-23 01:33:58 -0400

    Change split function by using numpy

--- code/utils/preprocess_dataset/set_functions.py
+++ code/utils/preprocess_dataset/set_functions.py
@@ -8,22 +8,16 @@ def import_data(path: str) -> np.ndarray:
     return np.array(df).T
 
 
-def split_set(train_set: np.ndarray, train_percenlabele: float,
-              test_percenlabele: float or None = None) -> (np.ndarray, np.ndarray):
-    if test_percenlabele is None:
-        test_percenlabele = 1.0 - train_percenlabele
-    if not (0.0 <= train_percenlabele <= 1.0 and
-            0.0 <= test_percenlabele <= 1.0 and
-            train_percenlabele + test_percenlabele <= 1.0):
-        raise ValueError("Incompatible percenlabele")
+def split_set(train_set: np.ndarray, train_percentage: float,
+              test_percentage: float or None = None) -> (np.ndarray, np.ndarray):
+    if test_percentage is None:
+        test_percentage = 1.0 - train_percentage
+    if not (0.0 <= train_percentage <= 1.0 and
+            0.0 <= test_percentage <= 1.0 and
+            train_percentage + test_percentage <= 1.0):
+        raise ValueError("Incompatible percentage")
     size = train_set.shape[-1]
-    train_size = round(size*train_percenlabele)
-    test_size = round(size*test_percenlabele)
-    train = []
-    test = []
-    to_split = list(train_set.T)
-    for i in range(train_size):
-        train.append(to_split.pop(randint(0, len(to_split) - 1)))
-    for i in range(test_size):
-        test.append(to_split.pop(randint(0, len(to_split) - 1)))
-    return np.array(train).T, np.array(test).T
+    train_size = round(size*train_percentage)
+    test_size = round(size*test_percentage)
+    to_split = np.random.permutation(train_set.T)
+    return to_split[0:train_size].T, to_split[train_size:train_size + test_size].T
