24044fdc993fa544e51f7ab87093ecf28a2d7392
StarBrand <juan.saez.hidalgo@gmail.com>
2019-10-04 11:22:58 -0300

    New engine template added

---
+++ code/genetic_algorithm/genetic_engine_optimized.py
@@ -0,0 +1,11 @@
+"""genetic_engine_optimized.py: GeneticEngineOptimized class"""
+from genetic_algorithm import GAEngine
+
+
+class GeneticEngineOptimized(GAEngine):
+    """
+    Genetic Algorithm engine, for multi objective individual (and his optimization)
+    and elitism
+    """
+    def __init__(self):
+        return
---
+++ tests/test_genetic_algorithm/test_engine_optimized.py
@@ -0,0 +1,18 @@
+"""test_engine_optimized.py: GeneticEngineOptimized unit test"""
+
+from unittest import TestCase, main
+
+
+class EngineOptimizedTest(TestCase):
+
+    def setUp(self) -> None:
+        """
+        Sets up test
+
+        :return:
+        """
+        pass
+
+
+if __name__ == '__main__':
+    main()
